7fbe8c230defaa81bf0c0a8ceb079ea107f41553
xaveerarif <xaveerarif@gmail.com>
2021-10-27 19:42:51 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBvpGYyLKg5o1MCEt76q81vpqPPkIGcNr4",
+    authDomain: "ema-jhon-c8441.firebaseapp.com",
+    projectId: "ema-jhon-c8441",
+    storageBucket: "ema-jhon-c8441.appspot.com",
+    messagingSenderId: "805000988865",
+    appId: "1:805000988865:web:49ac3374186686b880ec40"
+  };
+
+export default firebaseConfig;
